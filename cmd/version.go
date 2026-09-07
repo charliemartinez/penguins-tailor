0813ecc7a02527bfb2ff7e9dc5fca5a7423c3b3f
Piero Proietti <piero.proietti@gmail.com>
2026-09-07 08:45:23 +0200

    uffa

--- cmd/version.go
+++ cmd/version.go
@@ -13,7 +13,7 @@ func versionCmd() *cobra.Command {
 		Use:   "version",
 		Short: "Print tailor version",
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Printf("tailor %s - A \"tailor\" for you Prêt-à-porter costumes\n", Version)
+			fmt.Printf("tailor %s - A \"tailor\" for your Prêt-à-porter costumes\n", Version)
 		},
 	}
 }
